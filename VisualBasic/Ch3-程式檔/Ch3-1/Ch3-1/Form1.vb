02fee96c2d3e59559ca37a5bd1d6ddef5bda1a9b
alice2100chen <46878913+alice2100chen@users.noreply.github.com>
2019-02-03 12:37:46 +0800

    更新

---
+++ VisualBasic/Ch3-程式檔/Ch3-1/Ch3-1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Debug.Print("哈囉 資訊科技!")
+    End Sub
+End Class

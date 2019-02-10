ce00b7e11ba51ea990fc0e76370083cec4f8783c
alice2100chen <alice2100chen@gmail.com>
2019-02-10 15:49:42 +0800

    Fix incomplete comments

--- Python/Ch4-Py/Ch4-4.py
+++ Python/Ch4-Py/Ch4-4.py
@@ -5,7 +5,7 @@ num_array = [
 ]
 x = num_array[0]        # x會變成 [1, 2, 3]
 y = num_array[0][2]     # y會變成3
-num_array[2][1] = -8    # num_array[2][1]由8變成
+num_array[2][1] = -8    # num_array[2][1]由8變成-8
 print(x)
 print(y)
 print(num_array[2][1])

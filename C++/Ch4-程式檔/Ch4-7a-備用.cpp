457ede606593496eebfad8aac524bcb42044709f
LouisSung <sjf.csie@gmail.com>
2019-02-23 18:35:01 +0800

    delete old file

--- C++/Ch4-程式檔/Ch4-7a-備用.cpp
+++
@@ -1,4 +0,0 @@
-num_visitors = [70, 10, 14, 7, 25, 30, 50]
-day_names = ['日', '一', '二', '三', '四', '五', '六']
-for day_index in range(7):
-    print('星期'+day_names[day_index]+'：', num_visitors[day_index], '人')

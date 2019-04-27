523cbaf4b80bcf764a5cca5a66e9558fff062c15
alice2100chen <alice2100chen@gmail.com>
2019-04-27 11:15:09 +0800

    Modify the comment of printf to cout

--- C++-easy/Ch4-C++/Ch4-6a.cpp
+++ C++-easy/Ch4-C++/Ch4-6a.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     int score_array[4] = {95, 92, 88, 96};  // 陣列中有4個元素
-    // for迴圈會執行printf() 4次，因為0,1,2,3共有4個數值
+    // for迴圈會執行cout 4次，因為0,1,2,3共有4個數值
     // i在每一回合的值分別是0, 1, 2, 3
     for(int i = 0; i < 4; i++){
         cout << score_array[i] << endl;

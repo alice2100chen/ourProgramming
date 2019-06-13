b25199a0cde94984eae8b1b3427a43c4bb29b0a3
alice2100chen <alice2100chen@gmail.com>
2019-06-13 13:48:57 +0800

    code updated

--- Python/Ch5-Py/Ch5-1-bubbleSort-for-for.py
+++ Python/Ch5-Py/Ch5-1-bubbleSort-for-for.py
@@ -1,8 +1,8 @@
 #Ch5-1-bubbleSort-for-for.py
 #實作氣池排序演算法(雙迴圈)
 #版本: 1.0
-data = [4, 1, 3, 5, 2]                # 資料數 = 陣列長度 = 5個
-data_count = len(data)
+data = [4, 1, 3, 5, 2]
+data_count = len(data)                # 資料數 = 陣列長度 = 5個
 # 內層迴圈名稱(j)需和外層(i)不同
 for i in range(data_count-1):         # 5個數字，只需排4次，最後一個數會自動排好
     for j in range(data_count-1-i):   # 從第j個位置，開始往右比較 

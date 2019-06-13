e6d5f9af4353920b178d8fddb40c61de60350117
alice2100chen <alice2100chen@gmail.com>
2019-06-13 12:09:05 +0800

    更新程式

--- Python/Ch4-Py/Ch4E-4.py
+++ Python/Ch4-Py/Ch4E-4.py
@@ -1,12 +1,13 @@
-def gcd(p, q):
-    if p < q:
-        tmp = p
-        p = q
-        q = tmp
-    r = p % q
-    if r == 0:
-        return q
+def factorial(n):                  #函數名稱factorial及參數n
+    if n < 0:                      #函數內部的程式碼--由此開始
+        return '請輸入0或正整數'
+    elif n <=1:
+        return 1
     else:
-        return gcd(q, r)
-
-print(gcd(8, 12))
+        return n * factorial(n-1)  #函數內部的程式碼--到此結束
+    
+print (factorial(3))        #呼叫函數計算 3!
+print (factorial(10))       #呼叫函數計算 10!
+print (factorial(40))       #呼叫函數計算 40!
+print (factorial(0)) 
+print (factorial(-3))

54df519eb2289d543e95b886a9a35388a1bf68e7
alice2100chen <alice2100chen@gmail.com>
2019-07-09 21:38:46 +0800

    Update Form1.vb

--- VisualBasic/Ch4-VB-Windows-Form應用程式/Ch4-6a/Ch4-6a/Form1.vb
+++ VisualBasic/Ch4-VB-Windows-Form應用程式/Ch4-6a/Ch4-6a/Form1.vb
@@ -1,8 +1,8 @@
-﻿Public Class Form1
+Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim score_array() As Integer = {95, 92, 88, 96}       ' 陣列中有4個元素
-        ' for迴圈會執行print() 4次，因為陣列中有4個元素
+        ' for迴圈會執行Console.WriteLine() 4次，因為陣列中有4個元素
         ' i在每一回合的值分別是0, 1, 2, 3
         For i = 0 To 3
             Debug.Print(score_array(i))

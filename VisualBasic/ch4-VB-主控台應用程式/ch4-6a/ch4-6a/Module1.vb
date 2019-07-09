36d19aac64c63bd15d50e928b87e5c72171edf5c
alice2100chen <alice2100chen@gmail.com>
2019-07-09 21:37:16 +0800

    Update Module1.vb

--- VisualBasic/ch4-VB-主控台應用程式/ch4-6a/ch4-6a/Module1.vb
+++ VisualBasic/ch4-VB-主控台應用程式/ch4-6a/ch4-6a/Module1.vb
@@ -1,8 +1,8 @@
-﻿Module Module1
+Module Module1
 
     Sub Main()
         Dim score_array() As Integer = {95, 92, 88, 96}       ' 陣列中有4個元素
-        ' for迴圈會執行print() 4次，因為陣列中有4個元素
+        ' for迴圈會執行Console.WriteLine() 4次，因為陣列中有4個元素
         ' i在每一回合的值分別是0, 1, 2, 3
         For i = 0 To 3
             Console.WriteLine(score_array(i))

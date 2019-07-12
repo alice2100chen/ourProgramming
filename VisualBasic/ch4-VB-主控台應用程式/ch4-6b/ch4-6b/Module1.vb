46b06691fd90b28eafb8853158ef7c9973a6c4ac
alice2100chen <alice2100chen@gmail.com>
2019-07-12 14:13:26 +0800

    updated

--- VisualBasic/ch4-VB-主控台應用程式/ch4-6b/ch4-6b/Module1.vb
+++ VisualBasic/ch4-VB-主控台應用程式/ch4-6b/ch4-6b/Module1.vb
@@ -1,7 +1,7 @@
-﻿Module Module1
+Module Module1
 
     Sub Main()
-        Dim animals() As String = {"長頸鹿", "獅子", "老虎", "河馬", "熊", "兔子", "斑馬"}       ' 陣列中有4個元素
+        Dim animals() As String = {"長頸鹿", "獅子", "老虎", "河馬", "熊", "兔子", "斑馬"}       ' 陣列中有7個元素
         ' 因為陣列中有7個元素，for迴圈應會執行7次
         ' i在每一回合的值分別是0, 1, 2, 3, 4, 5, 6
         For i = 0 To 6

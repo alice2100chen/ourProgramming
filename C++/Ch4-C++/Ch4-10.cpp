65c801c68a03a35996cb93b8cc7b002200bab39f
alice2100chen <46878913+alice2100chen@users.noreply.github.com>
2019-09-14 22:11:47 +0800

    Create Ch4-10.cpp

---
+++ C++/Ch4-C++/Ch4-10.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+using namespace std;
+int main(){
+    int score = 92;
+    if(score >= 70){
+        cout << "通過" << endl;
+    }
+    else{
+        cout << "再試一次" << endl;
+    }
+    return 0;
+}
